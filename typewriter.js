cb369dbd742480cd3f71ca0524d877f766b981b2
Marcela Ang <marcela.ang@gmail.com>
2022-11-10 11:14:26 -0500

    M1W4 - Typewriter: Timeout function with delay

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from home";
+
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 50;
+} process.stdout.write('\n');
+
+
+
+
